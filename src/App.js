15e0b3caa96c6ab2ed08f55a3d0328fadf5eeef9
mohitrsyadav <mohitrsyadav@gmail.com>
2023-12-30 08:53:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "./components/Navbar";
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <h2>Food App</h2>
+    </div>
+  );
+}
+
+export default App;
